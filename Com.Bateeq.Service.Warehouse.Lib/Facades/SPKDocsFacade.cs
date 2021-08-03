e14d645c520c8ce0f14f6e1e8229ceaee2d82752
Fadhlinp <fadhli.nopri.pratama@gmail.com>
2021-08-04 06:35:19 +0700

    create Items, SPKDocs, & SPKDocItems

--- Com.Bateeq.Service.Warehouse.Lib/Facades/SPKDocsFacade.cs
+++ Com.Bateeq.Service.Warehouse.Lib/Facades/SPKDocsFacade.cs
@@ -14,6 +14,15 @@ using System.Linq.Dynamic.Core;
 using System.Text;
 using Com.Bateeq.Service.Warehouse.Lib;
 using Com.Bateeq.Service.Warehouse.Lib.Helpers;
+using System.Threading.Tasks;
+using Com.Bateeq.Service.Warehouse.Lib.ViewModels.SpkDocsViewModel;
+using Com.Bateeq.Service.Warehouse.Lib.Models.SPKDocsModel;
+using MongoDB.Bson;
+using HashidsNet;
+using Com.Bateeq.Service.Warehouse.Lib.ViewModels.NewIntegrationViewModel;
+using Com.Bateeq.Service.Warehouse.Lib.Interfaces;
+using System.Net.Http;
+using Com.Moonlay.Models;
 
 namespace Com.Bateeq.Service.Warehouse.Lib.Facades
 {
@@ -148,5 +157,319 @@ namespace Com.Bateeq.Service.Warehouse.Lib.Facades
         }
         #endregion
 
+        public async Task<int> Create(SPKDocsFromFinihsingOutsViewModel viewModel, string username, string token, int clientTimeZoneOffset = 7)
+        {
+            int Created = 0;
+
+            using (var transaction = this.dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    List<SPKDocsItem> sPKDocsItems = new List<SPKDocsItem>();
+                    int itemIdx = 1;
+                    foreach(var item in viewModel.Items) {
+
+                        // ambil product code, ambil first or default dari garment invoice detail yang product code dan no ro nya sama
+                        // lalu ambil invoiceItemid,
+                        // lalu ambil invoiceId, lalu ambil suplier
+
+                        //string itemsUri = "items/finished-goods";
+                        //var httpClient = (IHttpClientService)serviceProvider.GetService(typeof(IHttpClientService));
+                        //var response = await httpClient.PostAsync($"{APIEndpoint.Core}{itemsUri}", new StringContent(JsonConvert.SerializeObject(item).ToString(), Encoding.UTF8, General.JsonMediaType));
+
+                        //response.EnsureSuccessStatusCode();
+
+                        var isDifferentSize = item.IsDifferentSize;
+                        if (isDifferentSize == true)
+                        {
+                            int idx = 1;
+                            foreach(var detail in item.Details)
+                            {
+                                var sizeId = detail.Size.Id;
+                                var barcode = GenerateBarcode(idx,sizeId);
+                                var itemx = GetItem(barcode);
+
+                                if (itemx.Count() == 0 || itemx == null) //barcode belum terdaftar, insert ke tabel items (BMS) terlebih dahulu
+                                {
+                                    ItemCoreViewModelUsername itemCore = new ItemCoreViewModelUsername
+                                    {
+                                        dataDestination = new List<ItemViewModelRead>
+                                        {
+                                           new ItemViewModelRead
+                                           {
+                                               ArticleRealizationOrder = viewModel.RONo,
+                                               code = barcode,
+                                               name = viewModel.Comodity.name,
+                                               Size = item.Size.Size,
+                                               Uom = item.Uom.Unit,
+                                               ImagePath = viewModel.ImagePath,
+                                               ImgFile = "",
+                                               Tags = "",
+                                               Remark = "",
+                                               Description = "",
+                                               _id = 0
+                                           }
+                                        },
+                                        color = viewModel.color,
+                                        process = viewModel.process,
+                                        materials = viewModel.materials,
+                                        materialCompositions = viewModel.materialCompositions,
+                                        collections = viewModel.collections,
+                                        seasons = viewModel.seasons,
+                                        counters = viewModel.counters,
+                                        subCounters = viewModel.subCounters,
+                                        categories = viewModel.categories,
+                                        DomesticCOGS = item.BasicPrice,
+                                        DomesticRetail = 0,
+                                        DomesticSale = item.BasicPrice + item.ComodityPrice,
+                                        DomesticWholesale = 0,
+                                        InternationalCOGS = 0,
+                                        InternationalWholesale = 0,
+                                        InternationalRetail = 0,
+                                        InternationalSale = 0,
+                                        ImageFile = viewModel.ImgFile,
+                                        _id = 0,
+                                        Username = username,
+                                        Token = token
+                                    };
+
+                                    string itemsUri = "items/finished-goods";
+                                    var httpClient = (IHttpClientService)serviceProvider.GetService(typeof(IHttpClientService));
+                                    var response = await httpClient.PostAsync($"{APIEndpoint.Core}{itemsUri}", new StringContent(JsonConvert.SerializeObject(itemCore).ToString(), Encoding.UTF8, General.JsonMediaType));
+
+                                    response.EnsureSuccessStatusCode();
+
+                                    var item2 = GetItem(barcode);
+
+                                    sPKDocsItems.Add(new SPKDocsItem
+                                    {
+                                        ItemArticleRealizationOrder = viewModel.RONo,
+                                        ItemCode = barcode,
+                                        ItemDomesticCOGS = item.BasicPrice,
+                                        ItemDomesticSale = item.BasicPrice + item.ComodityPrice,
+                                        ItemId = item2.Single()._id,
+                                        ItemName = viewModel.Comodity.name,
+                                        ItemSize = item.Size.Size,
+                                        ItemUom = item.Uom.Unit,
+                                        Quantity = item.Quantity,
+                                        Remark = "",
+                                        SendQuantity = item.Quantity,
+                                    });
+                                }
+                                else // barcode sudah terdaftar
+                                {
+                                    sPKDocsItems.Add(new SPKDocsItem
+                                    {
+                                        ItemArticleRealizationOrder = viewModel.RONo,
+                                        ItemCode = barcode,
+                                        ItemDomesticCOGS = item.BasicPrice,
+                                        ItemDomesticSale = item.BasicPrice + item.ComodityPrice,
+                                        ItemId = itemx.Single()._id,
+                                        ItemName = viewModel.Comodity.name,
+                                        ItemSize = item.Size.Size,
+                                        ItemUom = item.Uom.Unit,
+                                        Quantity = item.Quantity,
+                                        Remark = "",
+                                        SendQuantity = item.Quantity,
+                                    });
+                                }
+                                idx++;
+                            }
+                        }
+                        else
+                        {
+                            var sizeId = item.Size.Id;
+                            var barcode = GenerateBarcode(itemIdx, sizeId);
+                            var itemx = GetItem(barcode);
+
+                            if (itemx.Count() == 0 || itemx == null) //barcode belum terdaftar, insert ke tabel items (BMS) terlebih dahulu
+                            {
+                                ItemCoreViewModelUsername itemCore = new ItemCoreViewModelUsername
+                                {
+                                    dataDestination = new List<ItemViewModelRead>
+                                        {
+                                           new ItemViewModelRead
+                                           {
+                                               ArticleRealizationOrder = viewModel.RONo,
+                                               code = barcode,
+                                               name = viewModel.Comodity.name,
+                                               Size = item.Size.Size,
+                                               Uom = item.Uom.Unit,
+                                               ImagePath = viewModel.ImagePath,
+                                               ImgFile = "",
+                                               Tags = "",
+                                               Remark = "",
+                                               Description = "",
+                                               _id = 0
+                                           }
+                                        },
+                                    color = viewModel.color,
+                                    process = viewModel.process,
+                                    materials = viewModel.materials,
+                                    materialCompositions = viewModel.materialCompositions,
+                                    collections = viewModel.collections,
+                                    seasons = viewModel.seasons,
+                                    counters = viewModel.counters,
+                                    subCounters = viewModel.subCounters,
+                                    categories = viewModel.categories,
+                                    DomesticCOGS = item.BasicPrice,
+                                    DomesticRetail = 0,
+                                    DomesticSale = item.BasicPrice + item.ComodityPrice,
+                                    DomesticWholesale = 0,
+                                    InternationalCOGS = 0,
+                                    InternationalWholesale = 0,
+                                    InternationalRetail = 0,
+                                    InternationalSale = 0,
+                                    ImageFile = "",
+                                    _id = 0,
+                                    Username = username,
+                                    Token = token
+                                };
+
+                                string itemsUri = "items/finished-goods/item";
+                                var httpClient = (IHttpClientService)serviceProvider.GetService(typeof(IHttpClientService));
+                                var response = await httpClient.PostAsync($"{APIEndpoint.Core}{itemsUri}", new StringContent(JsonConvert.SerializeObject(itemCore).ToString(), Encoding.UTF8, General.JsonMediaType));
+
+                                response.EnsureSuccessStatusCode();
+
+                                var item2 = GetItem(barcode);
+
+                                sPKDocsItems.Add(new SPKDocsItem
+                                {
+                                    ItemArticleRealizationOrder = viewModel.RONo,
+                                    ItemCode = barcode,
+                                    ItemDomesticCOGS = item.BasicPrice,
+                                    ItemDomesticSale = item.BasicPrice + item.ComodityPrice,
+                                    ItemId = item2.Single()._id,
+                                    ItemName = viewModel.Comodity.name,
+                                    ItemSize = item.Size.Size,
+                                    ItemUom = item.Uom.Unit,
+                                    Quantity = item.Quantity,
+                                    Remark = "",
+                                    SendQuantity = item.Quantity,
+                                });
+                            }
+                            else // barcode sudah terdaftar
+                            {
+                                sPKDocsItems.Add(new SPKDocsItem
+                                {
+                                    ItemArticleRealizationOrder = viewModel.RONo,
+                                    ItemCode = barcode,
+                                    ItemDomesticCOGS = item.BasicPrice,
+                                    ItemDomesticSale = item.BasicPrice + item.ComodityPrice,
+                                    ItemId = itemx.Single()._id,
+                                    ItemName = viewModel.Comodity.name,
+                                    ItemSize = item.Size.Size,
+                                    ItemUom = item.Uom.Unit,
+                                    Quantity = item.Quantity,
+                                    Remark = "",
+                                    SendQuantity = item.Quantity,
+                                });
+                            }
+                            itemIdx++;
+                        }
+                    }
+
+                    var packingListCode = GeneratePackingList();
+
+                    SPKDocs data = new SPKDocs()
+                    {
+                        Code = GenerateCode("EFR-PK/PBJ"),
+                        Date = viewModel.FinishingOutDate,
+                        DestinationId = (long)viewModel.UnitTo._id,
+                        DestinationCode = viewModel.UnitTo.code,
+                        DestinationName = viewModel.UnitTo.name,
+                        IsDistributed = true,
+                        IsReceived = false,
+                        PackingList = packingListCode,
+                        Password = "1",
+                        Reference = packingListCode,
+                        SourceId = (long)viewModel.Unit._id,
+                        SourceCode = viewModel.Unit.code,
+                        SourceName = viewModel.Unit.name,
+                        Weight = 0,
+                        Items = sPKDocsItems
+                    };
+
+                    foreach (var i in data.Items)
+                    {
+                        EntityExtension.FlagForCreate(i, username, USER_AGENT);
+                    }
+                    EntityExtension.FlagForCreate(data, username, USER_AGENT);
+                    dbContext.Add(data);
+
+                    Created = await dbContext.SaveChangesAsync();
+                    transaction.Commit();
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    throw new Exception(e.Message);
+                }
+            }
+
+            return Created;
+        }
+
+        public string GenerateCode(string ModuleId)
+        {
+            var uid = ObjectId.GenerateNewId().ToString();
+            var hashids = new Hashids(uid, 8, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+            var now = DateTime.Now;
+            var begin = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            var diff = (now - begin).Milliseconds;
+            string code = String.Format("{0}/{1}/{2}", hashids.Encode(diff), ModuleId, DateTime.Now.ToString("MM/yyyy"));
+            return code;
+        }
+
+        public string GenerateBarcode(int idx, int sizeId)
+        {
+            string code = "barcode "+idx+sizeId;
+            return code;
+        }
+
+        private List<ItemCoreViewModel> GetItem(string itemCode)
+        {
+            string itemUri = "items/finished-goods/Code";
+            IHttpClientService httpClient = (IHttpClientService)serviceProvider.GetService(typeof(IHttpClientService));
+
+            var response = httpClient.GetAsync($"{APIEndpoint.Core}{itemUri}/{itemCode}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(content);
+                List<ItemCoreViewModel> viewModel = JsonConvert.DeserializeObject<List<ItemCoreViewModel>>(result.GetValueOrDefault("data").ToString());
+                //return viewModel.OrderByDescending(s => s.Date).FirstOrDefault(s => s.Date < doDate.AddDays(1)); ;
+                return viewModel;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GeneratePackingList() // nomor urut/EFR-FN/bulan/tahun
+        {
+            var generatedNo = "";
+            var date = DateTime.Now;
+            var lastSPKDoc = dbContext.SPKDocs.OrderByDescending(entity => entity.Id).FirstOrDefault(entity => entity.PackingList.Contains("EFR-FN"));
+            string lastPackingListCode = "";
+
+            if (lastSPKDoc != null)
+            {
+                lastPackingListCode = lastSPKDoc.PackingList;
+                var code = lastPackingListCode.Split("/");
+                int nomorUrut = int.Parse(code.ElementAt(0));
+                nomorUrut++;
+
+                generatedNo = $"{nomorUrut.ToString("0000")}/EFR-FN/{date.ToString("MM")}/{date.ToString("yy")}";
+            }
+            else
+            {
+                generatedNo = $"0001/EFR-FN/{date.ToString("MM")}/{date.ToString("yy")}";
+            }
+
+            return generatedNo;
+        }
     }
 }
