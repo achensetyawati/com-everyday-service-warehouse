4f34c525ba7dc7d56238ae167a3a67f859c97ccd
yokana888 <yokana.adita@outlook.com>
2022-05-30 13:09:54 +0700

    change filter read pemasukan gudang pusat

--- Com.Bateeq.Service.Warehouse.Lib/Facades/TransferFacade.cs
+++ Com.Bateeq.Service.Warehouse.Lib/Facades/TransferFacade.cs
@@ -38,7 +38,7 @@ namespace Com.Bateeq.Service.Warehouse.Lib.Facades
 
         public Tuple<List<TransferInDoc>, int, Dictionary<string, string>> Read(int Page = 1, int Size = 25, string Order = "{}", string Keyword = null, string Filter = "{}")
         {
-            IQueryable<TransferInDoc> Query = this.dbSet.Include(m => m.Items).Where(m => m.Reference.Contains("EFR-FN"));
+            IQueryable<TransferInDoc> Query = this.dbSet.Include(m => m.Items).Where(m => m.Reference.Contains("EVR-FN"));
 
             List<string> searchAttributes = new List<string>()
             {
